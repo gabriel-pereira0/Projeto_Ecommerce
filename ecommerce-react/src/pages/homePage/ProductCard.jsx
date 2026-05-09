e043da48cf4fd6bf1bce713c79b493fd094d95a5
Gabriel Pereira <gabs.pereira0@icloud.com>
2026-05-09 15:22:50 -0300

    style: realizado ajuste no visual dos card

--- ecommerce-react/src/pages/homePage/ProductCard.jsx
+++ ecommerce-react/src/pages/homePage/ProductCard.jsx
@@ -6,24 +6,31 @@ import { useCartContext } from '../../contexts/CartContext';
 export function ProductCard({ id, name, brand, image, feminine, price }) {
   const { addToCart } = useCartContext();
   return (
-    <>
-      <article className='card-produto group w-48 bg-stone-100 shadow-xl shadow-slate-200 flex flex-col justify-around border-2 border-gray-200 rounded-lg'>
+    <article className='relative card-produto group w-60 shadow-xl flex flex-col border-2 border-gray-100 rounded-xl p-2 overflow-hidden'>
+      <div className='overflow-hidden rounded-xl'>
         <img
           src={image}
           alt={`Imagem do produto ${id}`}
-          className='group-hover:scale-110 rounded-lg mx-4 my-4 transition duration-300'
+          className='w-full object-cover transition-transform duration-300 group-hover:scale-105'
         />
-        <p className='mx-4 text-sm text-slate-400'>{brand}</p>
-        <p className='mx-4 text-sm '>{name}</p>
-        <p className='mx-4 text-sm text-green-700'>{price}</p>
+      </div>
+
+      <div className=' flex flex-1 flex-col justify-end transition-all duration-300 group-hover:-translate-y-2'>
+        <p className='text-m text-slate-400 group-hover:opacity-0'>{brand}</p>
+        <p className='text-m line-clamp-2 min-h-[30px]'>{name}</p>
+        <p className='text-lg text-green-700 group-hover:opacity-0'>
+          R$ {price}
+        </p>
+      </div>
+
+      <div className='absolute bottom-2 left-2 right-2 opacity-0 translate-y-full group-hover:translate-y-0 group-hover:opacity-100 transition-all duration-300'>
         <button
-          className='bg-slate-950 rounded-md mx-4 my-1 text-slate-100 hover:bg-slate-700'
+          className='bg-blue-700 rounded-xl text-white hover:bg-blue-600 w-full py-1'
           onClick={() => addToCart(id)}
         >
-          {' '}
           <FontAwesomeIcon icon={faCartPlus} />
         </button>
-      </article>
-    </>
+      </div>
+    </article>
   );
 }
